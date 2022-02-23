bbdc042ca3f0e5ac1dece9e69558c77f4a499c99
gcraia <100139902+gcraia@users.noreply.github.com>
2022-02-23 23:09:00 +0200

    Tester

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -4,5 +4,7 @@ public class Main {
     // JAR - Java Archive
     public static void main(String[] args) {
 	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
+
+        System.out.println("test");
     }
 }
