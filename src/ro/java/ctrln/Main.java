ef074c1ad99f7bb2764d883da6886102c4988f59
gcraia <100139902+gcraia@users.noreply.github.com>
2022-02-23 23:08:07 +0200

    Initial commit

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,8 @@
+package ro.java.ctrln;
+
+public class Main {
+    // JAR - Java Archive
+    public static void main(String[] args) {
+	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
+    }
+}
