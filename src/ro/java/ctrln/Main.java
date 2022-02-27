48fe22c4ff38a695b5e2915e503f1f91676f0dec
gcraia <100139902+gcraia@users.noreply.github.com>
2022-02-27 20:11:58 +0200

    Modified first println

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,7 @@ package ro.java.ctrln;
 public class Main {
     // JAR - Java Archive
     public static void main(String[] args) {
-	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
+	    System.out.println("Invat Java!");
 
         System.out.println("test");
     }
